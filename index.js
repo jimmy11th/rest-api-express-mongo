8a01a19ab23697d6d43ffaf1d7eb44ef4f10c32f
jimmy.chen <jimmy.chen@nirvana-info.com>
2023-07-03 18:54:59 +0800

    fix error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const SECRET = 'haiancs1993'
-const Account = require('../model/account')
+// const SECRET = 'haiancs1993'
+// const Account = require('../model/account')
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
@@ -17,22 +17,9 @@ database.once('connected', () => {
     console.log('Database Connected');
 })
 const app = express();
-const auth = async (req, res) => {
-    const raw = String(req.headers.authorization).split(' ').pop();
 
-    const { id } = jwt.verify(raw, SECRET);
-    req.user = await Account.findById(id)
-}
 app.use(express.json());
 app.use('/', routes);
-
-app.use(parseJwt({
-    secret: SECRET,
-    algorithms: ['HS256'], // 使用何种加密算法解析
-}).unless({ path: ['/api/login'] }))
-
-
-
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
